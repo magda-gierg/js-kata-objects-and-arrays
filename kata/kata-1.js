97b5875f30c6c5c06c230cd785d9ac3271f16541
magda-gierg <mjgiergiczny@gmail.com>
2017-09-28 10:24:32 +1300

    5 tests passed

--- kata/kata-1.js
+++ kata/kata-1.js
@@ -3,24 +3,37 @@
 //   key: value
 // }
 function makeObject (key, value) {
+  var result = {}
+  result[key] = value
+  return result
 }
 
 // getValue should return the value of the property contained in `key`
 function getValue (obj, key) {
+  return obj[key]
 }
 
 // ageOneYear should add one to the `age` property of `obj`
 function ageOneYear (obj) {
+  obj.age += 1
+  return obj
 }
 
 // deleteProp should remove the property contained in `key` from `obj`
 function deleteProp (obj, key) {
+  delete obj[key]
+  return obj
 }
 
 // makeArrayOfItem should return an array that is `length` long, made up of `item`
 // For example, makeArrayOfItem('foo', 2) would return:
 // ['foo', 'foo']
 function makeArrayOfItem (item, length) {
+  var newArray = []
+  for (var i=0; i< length; i++) {
+    newArray.push(item)
+  }
+  return newArray
 }
 
 // makeArrayOfItems should return an array containing all arguments passed to it
